55b08d73d82f8bbf55c25f43f10514917d22c2a2
ashdevelops <ash.smith.dev@gmail.com>
2025-02-19 19:35:07 +0000

    copy construct

--- gbuffer.cpp
+++ gbuffer.cpp
@@ -12,3 +12,32 @@ GapBuffer::GapBuffer(int gsize) : GAP_SIZE(gsize), buffer(NULL)
     InitBuffer(GAP_SIZE);
 
 };
+
+GapBuffer::GapBuffer(FILE *file, int gsize) : GAP_SIZE(gsize)
+{
+    struct stat buf;
+    
+    fstat(fileno(file), &buf);
+    long file_size = buf.st_size;
+    InitBuffer(file_size + GAP_SIZE);    
+    MoveGapToPoint();
+    ExpandGap( (int)file_size );
+    unsigned int amount = fread(gapstart, 1, file_size, file);
+
+    gapstart += amount;
+};
+
+GapBuffer::GapBuffer(const GapBuffer& tb)
+{
+     GAP_SIZE = tb.GAP_SIZE;
+     
+     buffer = (char *) malloc(tb.bufend - tb.buffer);
+     
+     strcpy(buffer, tb.buffer);
+
+     bufend = buffer + (tb.bufend - tb.buffer);
+     gapstart = buffer + (tb.gapstart - tb.buffer);
+     gapend = gapstart + (tb.gapend - tb.gapstart);
+     point = buffer + (tb.point - tb.buffer);
+    
+}
